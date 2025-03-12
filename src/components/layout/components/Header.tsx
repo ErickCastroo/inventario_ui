78096d8ef820a57edb1a7522781892c7e65449a1
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-12 09:58:20 -0700

    feat: Add Header component and integrate it into Layout

---
+++ src/components/layout/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className='bg-slate-800'>
+      <div className='mx-auto max-w-6xl py-10'>
+        <h1 className='text-4xl font-extrabold text-white'>Komito Storage</h1>
+        </div>
+    </header>
+
+  )
+}
+
+export { Header }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router"
+import { Outlet } from 'react-router'
+
+import { Header } from '@/components/layout/components/Header'
 
 function Layout() {
   return (
     <>
-      <h1>Layout</h1>
-      <Outlet />
+      <Header />
+      <main className='mt-10 mx-auto max-w-6xl p-10 bg-white shadow-lg '>
+        <Outlet />
+      </main>
     </>
   )
 }
